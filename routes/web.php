d777cad7311cab1a2c12eaa351510abce76e835c
matosluizdev <luizmatosdev@outlook.com>
2024-05-02 10:28:14 -0300

    fix: a importancia da ordem das rotas e prefixo para acesso

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,15 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\CheckIfIsAdmin;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group( function(){
+Route::middleware('auth')
+->prefix('admin')
+->group( function(){
     Route::delete('/users/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy')->middleware(CheckIfIsAdmin::class);
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::post('/users', [UserController::class, 'store'])->name('users.store');
     Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
 });
 
